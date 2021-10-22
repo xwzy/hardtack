6d06ab3367dce089a5df18ecd848c4753af6bc95
Zhenyuan Wang <zhenyuan_wang@foxmail.com>
2021-10-22 20:53:53 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// mod writer;
+
+
+use std::fs::File;
+use std::io::prelude::*;
+
+fn write_hello() -> std::io::Result<()> {
+    let mut file = File::create("foo.txt")?;
+    file.write_all(b"Hello");
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    write_hello();
 }
---
+++ src/writer.rs
@@ -0,0 +1,9 @@
+struct HTWriter {
+    
+}
+
+
+
+pub fn write_data() {
+    println!("write data");
+}
